0b9e0977f9eb5bea8ec6cfe59129a312b033d0d6
steven <stevenrobles147@gmail.com>
2025-12-11 08:52:27 -0500

    guardar

--- assets/js/script.js
+++
@@ -1,6 +0,0 @@
- const navToggle = document.getElementById("navToggle");
-  const navMenu = document.getElementById("navMenu");
-
-  navToggle.addEventListener("click", () => {
-    navMenu.classList.toggle("active");
-  });
